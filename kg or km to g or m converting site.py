c6f819e0265dfbe5c0fc3637454a19d49af6a3e7
adamossama1 <adamossama357@gmail.com>
2022-08-18 09:08:30 +0200

    python projects

---
+++ kg or km to g or m converting site.py
@@ -0,0 +1,43 @@
+
+
+
+username = 'hello world'
+password ='12345'
+
+if username ==input('enter username :')and password==input('enter password :'):
+        
+        x=0
+        while x<101:
+            print(x)
+            x+=1
+        
+ 
+
+        print('welcome...')
+
+        for letter in username:
+                
+                 
+                print(letter)
+                
+        k= int(input("enter kg or km :"))   
+
+        print(k*1000)
+
+        print("goodbye..")
+
+
+
+     
+else:
+        print('something wrong')                
+        
+               
+
+        
+
+
+
+
+
+
